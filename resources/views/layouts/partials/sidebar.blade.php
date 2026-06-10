bba7b038bcfe1b9efa039e86fa37e43b7a1e6469
Engel <chavarriaengel97@gmail.com>
2026-06-09 22:59:44 -0600

    improvements to the drop-down menu

--- resources/views/layouts/partials/sidebar.blade.php
+++ resources/views/layouts/partials/sidebar.blade.php
@@ -2,11 +2,11 @@
 {{--
     Cabecera del sidebar.
 --}}
-<div class="flex h-24 items-center justify-center px-3">
+<div class="flex h-24 items-center justify-center px-3 py-3 transition-[height] duration-500 ease-in-out lg:group-hover/sidebar:h-36">
     <img
         src="{{ asset('img/gnetlogo.png') }}"
         alt="Logo"
-        class="w-10 rounded-lg object-cover transition-all duration-500 ease-in-out lg:group-hover/sidebar:w-32"
+        class="max-h-full w-10 rounded-lg object-contain transition-all duration-500 ease-in-out lg:group-hover/sidebar:w-32"
     >
 </div>
 @php
@@ -22,7 +22,55 @@
     $puedeVerGestionTrabajadores = $puede( 2);
 @endphp
 
-<x-menu activate-by-route active-bg-color="!bg-[#2E8BC0] !text-white font-semibold shadow-sm">
+<x-menu
+    activate-by-route
+    active-bg-color="!bg-[#2E8BC0] !text-white font-semibold shadow-sm"
+    class="
+        [&_li>*]:transition-colors
+        [&_li>*]:duration-200
+
+        [&_li>*:hover]:!bg-[#EAF2FB]
+        [&_li>*:hover]:!text-[#1A2B42]
+
+        [&_li>*:active]:!bg-[#EAF2FB]
+        [&_li>*:active]:!text-[#1A2B42]
+
+        [&_li>*:focus]:!bg-[#EAF2FB]
+        [&_li>*:focus]:!text-[#1A2B42]
+
+        [&_li>*:focus-visible]:!outline-none
+        [&_li>*:focus-visible]:!ring-2
+        [&_li>*:focus-visible]:!ring-[#2E8BC0]/25
+
+        [&_summary]:transition-colors
+        [&_summary]:duration-200
+
+        [&_summary:hover]:!bg-[#EAF2FB]
+        [&_summary:hover]:!text-[#1A2B42]
+
+        [&_summary:active]:!bg-[#EAF2FB]
+        [&_summary:active]:!text-[#1A2B42]
+
+        [&_summary:focus]:!bg-[#EAF2FB]
+        [&_summary:focus]:!text-[#1A2B42]
+
+        [&_summary:focus-visible]:!outline-none
+        [&_summary:focus-visible]:!ring-2
+        [&_summary:focus-visible]:!ring-[#2E8BC0]/25
+
+        [&_li>.active]:!bg-[#2E8BC0]
+        [&_li>.active]:!text-white
+        [&_li>.active:hover]:!bg-[#2E8BC0]
+        [&_li>.active:active]:!bg-[#2E8BC0]
+        [&_li>.active:focus]:!bg-[#2E8BC0]
+
+        [&_summary.active]:!bg-[#2E8BC0]
+        [&_summary.active]:!text-white
+        [&_summary.active:hover]:!bg-[#2E8BC0]
+        [&_summary.active:active]:!bg-[#2E8BC0]
+        [&_summary.active:focus]:!bg-[#2E8BC0]
+    "
+>
 
     {{-- Ventas: cargos 1  2 3  --}}
     @if ($puedeVerVentas)
